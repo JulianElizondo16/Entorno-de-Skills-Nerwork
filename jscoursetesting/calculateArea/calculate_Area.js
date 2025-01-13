38ebdf20b6543a4c8e9466de63e3b86862b80357
Julian Elizondo <JulianECPersonal@gmail.com>
2025-01-12 22:04:14 -0600

    Laboratorio Práctico: Cómo escribir su primera función en Javascript

--- jscoursetesting/calculateArea/calculate_Area.js
+++ jscoursetesting/calculateArea/calculate_Area.js
@@ -6,5 +6,22 @@ function calculateArea() {
   width = parseFloat(document.getElementById("width").value); //.value se usa para recuperar el dato enviado desde el html
 
   let area = length * width;
-  document.getElementById('result').innerText = `The area of the rectangle is: ${area}`;
+  document.getElementById(
+    "result"
+  ).innerText = `The area of the rectangle is: ${area}`;
+}
+
+let grocery1;
+let grocery2;
+let grocery3;
+
+function groceryTracker() {
+  grocery1 = parseFloat(document.getElementById("grocery1").value); //.value se usa para recuperar el dato enviado desde el html
+  grocery2 = parseFloat(document.getElementById("grocery2").value); //.value se usa para recuperar el dato enviado desde el html
+  grocery3 = parseFloat(document.getElementById("grocery3").value); //.value se usa para recuperar el dato enviado desde el html
+
+  let total = grocery1 + grocery2 + grocery3;
+  document.getElementById(
+    "resultado"
+  ).innerText = `El monto Total es: $${total}`;
 }
