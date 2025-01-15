18de85eb9ddb7d38a93c0d0c1d8b1574f533f738
Julian Elizondo <JulianECPersonal@gmail.com>
2025-01-14 22:09:50 -0600

    Laboratorio Práctico: Visualización de la información de los empleados mediante métodos de matriz

---
+++ jscoursetesting/employeeDetails/employee_details.js
@@ -0,0 +1,52 @@
+const employees = [
+  { id: 1, name: "John Doe", age: 30, department: "IT", salary: 50000 },
+  { id: 2, name: "Alice Smith", age: 28, department: "HR", salary: 45000 },
+  { id: 3, name: "Bob Johnson", age: 35, department: "Finance", salary: 60000 },
+  //... Se pueden añadir más registros de empleados aquí
+];
+
+// Función para mostrar todos los empleados
+//El método map() en JavaScript se utiliza para crear un nuevo array con los resultados de aplicar una función a cada uno de los elementos del array original. En el contexto de tu código, map() se está utilizando para iterar sobre un array llamado employees y aplicar una función a cada employee.
+
+const totalEmployees = employees
+  .map(
+    (employee, index) =>
+      `<p>${employee.id}: ${employee.name}: ${employee.name} - ${employee.department} - $${employee.salary}</p>`
+  )
+  .join("");
+document.getElementById("employeesDetails").innerHTML = totalEmployees;
+
+function calculateTotalSalaries() {
+  const totalSalaries = employees.reduce(
+    (acc, employee) => acc + employee.salary,
+    0
+  );
+  alert(`Total Salaries: $${totalSalaries}`);
+}
+
+function displayHREmployees() {
+  const hrEmployees = employees.filter(
+    (employee) => employee.department === "HR"
+  );
+  const hrEmployeesDisplay = hrEmployees
+    .map(
+      (employee, index) =>
+        `<p>${employee.id}: ${employee.name}: ${employee.name} - ${employee.department} - $${employee.salary}</p>`
+    )
+    .join("");
+  document.getElementById("employeesDetails").innerHTML = hrEmployeesDisplay;
+}
+
+function findEmployeeById(employeeId) {
+  const foundEmployee = employees.find(
+    (employee) => employee.id === employeeId
+  );
+  if (foundEmployee) {
+    document.getElementById(
+      "employeesDetails"
+    ).innerHTML = `<p>${foundEmployee.id}: ${foundEmployee.name}: ${foundEmployee.name} - ${foundEmployee.department} - $${foundEmployee.salary}</p>`;
+  } else {
+    document.getElementById("employeesDetails").innerHTML =
+      "no se ha encontrado ningún empleado con este ID";
+  }
+}
